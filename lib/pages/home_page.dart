b6dd5979bd27370f459a3c5da540045e7fd92ed7
Dang Quang Loi <isow.dev@gmail.com>
2024-01-20 21:57:13 +0700

    fix screen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,25 +8,97 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final orientation = MediaQuery.of(context).orientation;
-
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: kColorPrimary,
       body: SafeArea(
         child: orientation == Orientation.portrait
-            ? const Stack(children: [
-                GetStartedBackground(),
-                FractionallySizedBox(
-                  heightFactor: 0.4,
+            ? Stack(children: [
+                const GetStartedBackground(),
+                const FractionallySizedBox(
+                  heightFactor: 0.35,
                   child: GetStartedHeader(),
                 ),
+                Align(
+                  alignment: const Alignment(0.0, 0.8),
+                  child: GetStartedButton(
+                    fixedSize: MaterialStateProperty.all(
+                      Size(
+                        size.width * 0.9,
+                        size.height * 0.065,
+                      ),
+                    ),
+                    textStyle: MaterialStateProperty.all(
+                      PrimaryFont.medium(size.height * 0.015),
+                    ),
+                  ),
+                )
               ])
-            : const Row(
+            : Row(
                 children: [
-                  Expanded(child: GetStartedHeader()),
-                  Expanded(child: GetStartedBackground()),
+                  const Expanded(
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: FractionallySizedBox(
+                        heightFactor: 0.8,
+                        child: GetStartedHeader(),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Stack(
+                      children: [
+                        const GetStartedBackground(),
+                        Align(
+                          alignment: const Alignment(0.0, 0.8),
+                          child: GetStartedButton(
+                            fixedSize: MaterialStateProperty.all(
+                              Size(
+                                size.width * 0.4,
+                                size.height * 0.065,
+                              ),
+                            ),
+                            textStyle: MaterialStateProperty.all(
+                              PrimaryFont.medium(size.height * 0.015),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
       ),
+    );
+  }
+}
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({
+    super.key,
+    this.fixedSize,
+    this.textStyle,
+  });
+
+  final MaterialStateProperty<Size>? fixedSize;
+  final MaterialStateProperty<TextStyle>? textStyle;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(kColorLightGrey),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(38),
+          ),
+        ),
+        elevation: MaterialStateProperty.all(0),
+        fixedSize: fixedSize,
+        foregroundColor: MaterialStateProperty.all(kColorDarkGrey),
+        textStyle: textStyle,
+      ),
+      child: const Text('GET STARTED'),
     );
   }
 }
@@ -38,12 +110,14 @@ class GetStartedBackground extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
     return Align(
       alignment: Alignment.bottomCenter,
       child: FractionallySizedBox(
-        heightFactor: 0.6,
+        heightFactor: orientation == Orientation.portrait ? 0.6 : 1,
         widthFactor: 1,
         child: FittedBox(
+          alignment: Alignment.topCenter,
           fit: BoxFit.cover,
           child: SvgPicture.asset('assets/images/bg_get_started.svg'),
         ),
@@ -58,42 +132,60 @@ class GetStartedHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(
+        Flexible(
+          flex: 2,
+          fit: FlexFit.tight,
           child: SvgPicture.asset(
             'assets/images/ic_logo.svg',
             alignment: const Alignment(0.0, -0.8),
           ),
         ),
-        Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              RichText(
-                text: TextSpan(
-                  text: 'Hi Afsar, Welcome\n',
-                  style: PrimaryFont.medium(30).copyWith(
-                    color: kColorLightYellow,
-                    height: 1.3,
-                  ),
-                  children: [
-                    TextSpan(
-                      text: 'to Silent Moon',
-                      style: PrimaryFont.thin(30).copyWith(
-                        color: kColorLightYellow,
-                      ),
+        Flexible(
+          flex: 1,
+          fit: FlexFit.tight,
+          child: FittedBox(
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                text: 'Hi Afsar, Welcome\n',
+                style: PrimaryFont.medium(30).copyWith(
+                  color: kColorLightYellow,
+                  height: 1.3,
+                ),
+                children: [
+                  TextSpan(
+                    text: 'to Silent Moon',
+                    style: PrimaryFont.thin(30).copyWith(
+                      color: kColorLightYellow,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              Text(
-                'Explore the app, Find some peace of mind to prepare for meditation.',
-                style: PrimaryFont.light(16).copyWith(
-                  color: kColorLightGrey,
-                ),
-              ),
-            ],
+            ),
           ),
-        )
+        ),
+        Flexible(
+          flex: 1,
+          fit: FlexFit.tight,
+          child: SizedBox.expand(
+            child: FractionallySizedBox(
+              widthFactor: 0.8,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: FittedBox(
+                  child: Text(
+                    'Explore the app, Find some peace of mind\n to prepare for meditation.',
+                    style: PrimaryFont.light(16).copyWith(
+                      color: kColorLightGrey,
+                      height: 1.5,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
